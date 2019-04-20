2b3182727b43302b0acb30a487f05d386a41d64d
oudomso <49217163+oudomso@users.noreply.github.com>
2019-04-20 13:03:44 -0500

    add js code for game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,3 +2,29 @@
 
 //array of word for the hangman
 var answer = ["GALAGA","PAC MAN", "DONKEY KONG", "FROGGER", "DIG DUG", "DEFENDER", "TETRIS"];
+
+
+function Game() {
+    //computer generates random word from words array
+    randomWord = words[Math.floor(Math.random() * words.length)];
+
+    // split the individual word into separate arrays, and store in new array 
+    lettersOfWord = randomWord.split("");
+
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length;
+
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    //showing the "_" within HTML
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+
+    //console logging 
+    console.log(randomWord);
+    console.log(lettersOfWord)
+    console.log(blanks)
+    console.log(blanksAndCorrect)
+}
